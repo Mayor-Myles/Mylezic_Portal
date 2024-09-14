2ab65911a68d038d2d4c03c7bb0cfe5f3d792cba
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-14 13:41:32 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,112 +1,50 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {ChakraProvider,useToast} from "@chakra-ui/react";
-import {RecoilRoot,useRecoilState} from "recoil";
-import {csrfState,userData,dataPlansState} from "../states/recoil";
-import {useEffect} from "react";
-
-
-
+import { ChakraProvider, useToast } from "@chakra-ui/react";
+import { RecoilRoot, useRecoilState } from "recoil";
+import { csrfState, userData, dataPlansState } from "../states/recoil";
+import { useEffect } from "react";
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return(
+  return (
     <RecoilRoot>
-    <ChakraProvider>
-    <InitializeState /> 
-  <Component {...pageProps} />
-</ChakraProvider>
+      <ChakraProvider>
+        <InitializeState />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </RecoilRoot>
-    )
+  )
 }
 
-
-function InitializeState(){
-
-const [user,setUser] = useRecoilState(userData);
-
-  const [csrf,setCsrf] = useRecoilState(csrfState);
-
-  const [plan,setPlan] = useRecoilState(dataPlansState);
-
-
+function InitializeState() {
+  const [user, setUser] = useRecoilState(userData);
+  const [csrf, setCsrf] = useRecoilState(csrfState);
+  const [plan, setPlan] = useRecoilState(dataPlansState);
   const toast = useToast();
 
-  
   useEffect(() => {
+    const getToken = async () => {
+      const url = "https://cbrbakery.com.ng/api?action=welcome";
 
-/*const getDataPlans = async () => {
+      try {
+        const response = await axios.get(url);
 
-const url = "https://cbrbakery.com.ng/api?action=getDataPlans";
-  
-  
-  const response = await 
-
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      //body: JSON.stringify(formData),
-    })
-      .then(res => {
-        if (!res.ok) {
-         // setLoading(false);
-          throw new Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then(data => {
-        //setCsrf(data.token);
-        //setLoading(false);
-        if (data.status === "success") {
-       
-   setPlan(data.dataPlans);
-      
-        } 
-          
-      })
-      .catch(error => {
-        console.log(error)
-    
-      });
-    
-  
-}*/
-
-
-const getToken = async () => {
-
-const url = "https://cbrbakery.com.ng/api?action=welcome";
-
-  const response = await 
-  
-fetch(url)
-      
-      .then(res => {
-        if (!res.ok) {
-          
-          throw new Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then(data => {
-        
-        if(data) {
-       console.log(data)
-   setCsrf(data.token);
-   setUser(data.userData && data.userData);     
-  setPlan(data.dataPlans && data.dataPlans);
+        if (response.data) {
+          console.log(response.data);
+          setCsrf(response.data.token);
+          setUser(response.data.userData || null);
+          setPlan(response.data.dataPlans || null);
         } else {
           toast({
             title: "Error",
-            description:"Page is expired. Please reload the page.",
+            description: "Page is expired. Please reload the page.",
             status: "error",
             duration: 5000,
             isClosable: true,
           });
         }
-      })
-      .catch(error => {
+      } catch (error: any) {
         toast({
           title: "Error",
           description: "Your request could not be processed. " + error.message,
@@ -115,75 +53,14 @@ fetch(url)
           isClosable: true,
           position: "top",
         });
-        
-      });
-  
+      }
+    }
 
-  
-  
-  
-}
-
-/*
-    const getUserData = async () =>{
-
-        const url = "https://cbrbakery.com.ng/api?action=getUserData";
-
-        const response = await
-        fetch(url)
-
-        .then(res => {
-          if (!res.ok) {
-
-            throw new Error(res.statusText);
-          }
-          return res.json();
-        })
-        .then(data => {
-
-          if(data) {
-
-     setUser(data.userData);
-
-          } else {
-            toast({
-              title: "Error",
-              description:"Page is expired. Please reload the page.",
-              status: "error",
-              duration: 5000,
-              isClosable: true,
-            });
-          }
-        })
-        .catch(error => {
-          toast({
-            title: "Error",
-            description: "Your request could not be processed. " + error.message,
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-            position: "top",
-          });
-
-        });
-
-      }*/
-
-    
-    //Fetch data plan from db
-   // getDataPlans();
-
-    //getCsrf
     getToken();
+  }, [setCsrf, setUser, setPlan, toast]);
 
-    //fetch user Data
-  //  getUserData();
-
-    
-    
-  })
-  
   return null;
 }
 
-export default MyApp
+export default MyApp;
+              
