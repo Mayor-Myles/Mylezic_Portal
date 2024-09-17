6ac501715d1d611ef633b4a3fa6f5b9bcf6a2139
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-17 10:15:01 +0100

    Update transactions.js

--- components/transactions.js
+++ components/transactions.js
@@ -14,7 +14,7 @@ import {useRecoilState} from "recoil";
 
 
 const TransactionItem = ({ icon, name, type, amount ,color,date,tid}) => (
-  <HStack gap="4" justify="space-between" w="full" py={2}>
+  <HStack gap="5" justify="space-between" w="fulll" py={2}>
     
       <Icon color="seagreen" as={icon} boxSize={4} mr="3" />
       <VStack mr={3} align="start" spacing={0}>
