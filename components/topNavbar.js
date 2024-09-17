6a9319460558966e9f34d60ce2fa1aff2a15f2d0
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-17 08:55:04 +0100

    Update topNavbar.js

--- components/topNavbar.js
+++ components/topNavbar.js
@@ -19,7 +19,7 @@ const { colorMode, toggleColorMode } = useColorMode();
       position="fixed"
       top="0"
       width="100%"
-      bg={colorMode === "light" && "white"}
+      bg={colorMode === "light" ? "white" : "grey.900"}
       mb="1em"
       zIndex="999"
       p="2"
