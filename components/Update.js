b47ca1e95c668dd650263dccd6a1469bd4f4cd1e
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-23 21:01:11 +0100

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -1,1 +1,47 @@
+import axios from "axios";
+import { userData } from "../states/recoil";
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
 
+const Update = () => {
+  const [user, setUser] = useRecoilState(userData);
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = "https://cbrbakery.com.ng/api/welcome";
+      
+      try {
+        const response = await axios.get(url);
+        
+        if (response.data && response.data.userData) {
+          setUser(response.data.userData);
+        } else {
+          toast({
+            title: "Error",
+            description: "Page is expired. Please reload the page.",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          });
+        }
+      } catch (error) {
+        toast({
+          title: "Error",
+          description: "Failed to fetch data. Please try again later.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      }
+    };
+
+    fetchData();
+  }, [toast, setUser]);
+
+  return null;
+};
+
+export default Update;
+  
