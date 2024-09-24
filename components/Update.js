36ae60834cf9fb92a1d646807ccba77926a4d47e
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-24 05:04:47 +0100

    Update Update.js

--- components/Update.js
+++ components/Update.js
@@ -1,48 +1,42 @@
 import axios from "axios";
 import { userData } from "../states/recoil";
 import { useRecoilState } from "recoil";
-import { useEffect } from "react";
 import { useToast } from "@chakra-ui/react";
 
-const Update = () => {
+const useUpdate = () => {
   const [user, setUser] = useRecoilState(userData);
   const toast = useToast();
 
-  
-    const fetchData = async () => {
-      const url = "https://cbrbakery.com.ng/api/welcome";
-      
-      try {
-        const response = await axios.get(url);
-        
-        if (response.data && response.data.userData) {
-          setUser(response.data.userData);
-        } else {
-          toast({
-            title: "Error",
-            description: "Page is expired. Please reload the page.",
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-            position:'top',
-          });
-        }
-      } catch (error) {
+  const update= async () => {
+    const url = "https://cbrbakery.com.ng/api/welcome";
+    
+    try {
+      const response = await axios.get(url);
+
+      if (response.data && response.data.userData) {
+        setUser(response.data.userData);
+      } else {
         toast({
           title: "Error",
-          description: "Failed to fetch data. Please try again later.",
+          description: "Page is expired. Please reload the page.",
           status: "error",
           duration: 5000,
           isClosable: true,
         });
       }
-    };
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Failed to fetch data. Please try again later.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
-    fetchData();
-  
-
-  return null;
+  return update;
 };
 
-export default Update;
-  
+export default useUpdateUserData;
+        
