35751f909e05669723fd335970cf29a80f6d68a4
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-26 17:46:27 +0100

    Update notifications.js

--- components/notifications.js
+++ components/notifications.js
@@ -14,10 +14,9 @@ import {
 import {notificationsState} from "../states/recoil";
 import {useRecoilState} from "recoil";
 
-
 function Notification({ opened: { opened, setOpened } }) {
   
-const [messages,setMessages] = useRecoilState(notificationsState);
+  const [messages, setMessages] = useRecoilState(notificationsState);
   
   return (
     <Modal isOpen={opened} onClose={() => setOpened(false)}>
@@ -29,12 +28,16 @@ const [messages,setMessages] = useRecoilState(notificationsState);
             <Box p="5" borderRadius="md" boxShadow="lg" color="white" bg="teal.400" key={index} mb={10}>
               <Heading mb="2" textDecoration="underline" size="sm">{msg.title}</Heading>
               <Text>{msg.message}</Text>
+              
+              <Text fontSize="sm" mt="2" color="gray.300">
+                {msg.date}
+              </Text>
             </Box>
           ))}
         </ModalBody>
         <ModalFooter>
           <Button colorScheme="red" onClick={() => setOpened(false)}>
-            close
+            Close
           </Button>
         </ModalFooter>
       </ModalContent>
