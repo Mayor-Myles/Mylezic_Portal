3826925b114a66aee7f269b5f985a5486c932968
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-26 15:00:05 +0100

    Create notifications.js

---
+++ components/notifications.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Box,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+
+function Notification() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const messages = [
+    {
+      title: 'New data plan',
+      message: 'We now have a good day at work and I will try to get the rest of the day.',
+    },
+    {
+      title: 'System update',
+      message: 'A new system update is available. Please update to the latest version.',
+    },
+  ];
+
+  return (
+    <>
+
+      <Modal isOpen={true} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Notifications</ModalHeader>
+          <ModalBody>
+            {messages.map((msg, index) => (
+              <Box key={index} mb={4}>
+                <Heading size="md">{msg.title}</Heading>
+                <Text>{msg.message}</Text>
+              </Box>
+            ))}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="red.400" onClick={onClose}>close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default Notification;
