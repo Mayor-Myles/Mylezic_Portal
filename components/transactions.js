fe7ac8f3aa11dedd155e6942c6237f0e9c42c256
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-27 14:16:43 +0100

    Update transactions.js

--- components/transactions.js
+++ components/transactions.js
@@ -13,19 +13,25 @@ import {useRecoilState} from "recoil";
 
 
 
-const TransactionItem = ({ icon, name, type, amount ,color,date,tid}) => (
-  <HStack gap="3" justify="space-between" w="fulll">
-    
-  
-  <Text fontSize="sm" color="gray.500"> <Icon color="seagreen" as={icon} boxSize={4} ml="2" /></Text>
- <Text fontSize="sm" color="gray.500">{name} - {type}</Text>
- <Text fontSize="13px" ml={2} color="gray.400">REF - {tid}</Text>
-  <Text color={color} fontSize="14px">₦{amount.toLocaleString()}</Text>
-  <Text fontSize="13px" ml={2} color="gray.400">{date}</Text>
-      
-  
+const TransactionItem = ({ icon, name, type, amount, color, date, tid }) => (
+  <HStack gap="3" justify="space-between" w="full" wrap="wrap">
+    <HStack minW="20%" spacing={2}>
+      <Icon color="seagreen" as={icon} boxSize={4} ml="2" />
+      <Text fontSize="sm" color="gray.500" isTruncated>{name} - {type}</Text>
+    </HStack>
+
+    <Text fontSize="13px" ml={2} color="gray.400" isTruncated>REF - {tid}</Text>
+
+    <Text color={color} fontSize="14px" whiteSpace="nowrap" isTruncated>
+      ₦{amount.toLocaleString()}
+    </Text>
+
+    <Text fontSize="13px" ml={2} color="gray.400" isTruncated>
+      {date}
+    </Text>
   </HStack>
 );
+
 
 const Transaction = () => {
 
