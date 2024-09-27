b1fdf9dbacb3da8e7c21e3094034e8fa950cc1ff
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-27 15:00:30 +0100

    Update transactions.js

--- components/transactions.js
+++ components/transactions.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import { Box, Text, HStack,VStack, Icon, Flex, SimpleGrid } from '@chakra-ui/react';
+import { MdCellWifi,} from 'react-icons/md';
+import { FiPhoneCall,} from 'react-icons/fi';
+import {FaRegMessage} from "react-icons/fa6";
+import {FaExchangeAlt} from "react-icons/fa";
+import { IoPersonOutline } from "react-icons/io5";
+import {userData} from "../states/recoil";
+import {useRecoilState} from "recoil";
+
+
+
 const TransactionItem = ({ icon, name, type, amount, color, date, tid }) => (
   <HStack gap="3" justify="space-between" w="full" wrap="wrap">
     <HStack minW="20%" spacing={2}>
@@ -16,4 +28,80 @@ const TransactionItem = ({ icon, name, type, amount, color, date, tid }) => (
     </Text>
   </HStack>
 );
+
   
+
+
+
+const Transaction = () => {
+
+const [user,setUser] = useRecoilState(userData);
+
+const icons = {
+
+"data":MdCellWifi,
+  "airtime":FiPhoneCall,
+  "bulkSMS":FaRegMessage,
+  "a2c":FaExchangeAlt,
+  "hire":IoPersonOutline,
+  
+  
+}
+
+  const colors = {
+
+    "success" : "teal",
+    "error" : "red",
+    "pending": "orange",
+  }
+  
+  
+  return (
+    
+    <Box display="flex" flexFlow="column" justify="center" align="center" mb="5em" mt="0.5em" p="1" w="full">
+      
+      
+        <Text fontSize="1em" fontWeight="bold">
+          Transactions History 
+        </Text>
+
+
+    <Flex align="center" justify="space-around" w="100%">
+      {/* Error State */}
+      <Flex align="center">
+        <Box w="10px" h="10px" bg="red.500" borderRadius="50%" mr="2" />
+        <Text>Error</Text>
+      </Flex>
+
+      {/* Pending State */}
+      <Flex align="center">
+        <Box w="10px" h="10px" bg="orange.400" borderRadius="50%" mr="2" />
+        <Text>Pending</Text>
+      </Flex>
+
+      {/* Success State */}
+      <Flex align="center">
+        <Box w="10px" h="10px" bg="teal.400" borderRadius="50%" mr="2" />
+        <Text>Success</Text>
+      </Flex>
+
+  </Flex>
+        <VStack spacing={4} align="center">
+
+          {user.transactions ? user.transactions.map((transaction,index) => (
+      
+        
+<TransactionItem key={index} icon={icons[transaction.icon]} name={transaction.title} type={transaction.description} amount={transaction.amount} color={colors[transaction.status]} date={transaction.date} tid={transaction.tid}/>
+
+    )) :
+
+         (<Text color="grey">You have not made any transactions. You can buy airtime,data, hire us and your transactions would show up here.</Text>)}
+        
+          
+        </VStack>
+    
+    </Box>
+  );
+};
+
+export default Transaction;
