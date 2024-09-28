419867bd0a286736c2df5c4aa72b6187ddd6df10
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2024-09-28 15:09:38 +0100

    Update bottomNavbar.js

--- components/bottomNavbar.js
+++ components/bottomNavbar.js
@@ -65,17 +65,18 @@ return;
 <Text color="grey" fontSize="12px">{colorMode==="light" ? "Dark Mode" : "Light Mode"}</Text>
       </Box>
 
-      <Box onClick={()=>window.open(`https://wa.me/2347014443158?text=Hi%20Mylezic%20support%2C%20I%20need%20an%20assistant%20from%20you.%20My%20user id%20is%20 ${user.userId}`,'_blank')}>
-      <IconButton
-        aria-label="support"
-        icon={<Icon as={AiOutlineCustomerService} fontSize="" />}
-        fontSize="1.8rem"
-        variant="ghost"
-      />
-<Text color="grey" fontSize="12px">
+      <Box onClick={() => window.open(`https://wa.me/2347014443158?text=Hi%20Mylezic%20support%2C%20I%20need%20an%20assistant%20from%20you.%20My%20user%20id%20is%20${encodeURIComponent(user.userId)}`, '_blank')}>
+  <IconButton
+    aria-label="support"
+    icon={<Icon as={AiOutlineCustomerService} />}
+    fontSize="1.8rem"
+    variant="ghost"
+  />
+  <Text color="grey" fontSize="12px">
     Support
   </Text>
-    </Box>
+</Box>
+
       <Box onClick={()=>router.push("/dashboard")}
         display="flex"
         alignItems="center"
